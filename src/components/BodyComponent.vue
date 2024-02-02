5874975b366ff1502aa162b597cc55e7946f9b58
dmg04144 <dmg04144@naver.com>
2024-02-02 18:01:34 +0900

    02/02

---
+++ src/components/BodyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+        <ul id="myUL">
+            <li v-for="tod in list" v-bind:key="tod.no" @click="cancleTodo(tod.cancelFlag)">{{ tod.todo }}<span class="close"  @click="deleteTodo(tod.no)">X</span></li>
+        </ul>
+</template>
+
+<script>
+
+export default{ 
+    props: ['list'],
+    methods:{
+        deleteTodo(no) { 
+            this.$emit('todo-delete',no)
+        },
+        cancleTodo(cancelFlag) {
+            // if(!this.cancelFlag){
+                
+            // }else{
+
+            // }
+            this.$emit('todo-cancle',cancelFlag)
+        }
+    }
+}
+</script>
